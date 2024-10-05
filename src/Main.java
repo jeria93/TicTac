388a13a953246b229829b8cf8c037f8939f6961f
nicholassamuelssonjeria <nicholasjobb.jeria@gmail.com>
2024-10-05 11:55:15 +0200

    First draft on building the game, board seems to coming in nice.

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,37 @@
+import java.awt.datatransfer.Clipboard;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        String [][] cars = new String[3][3];
+        String[][] board = new String[3][3];
+        board[0][0] = "";
+        board[0][1] = "";
+        board[0][2] = "";
+        board[1][0] = "";
+        board[1][1] = "";
+        board[1][2] = "";
+        board[2][0] = "";
+        board[2][1] = "";
+        board[2][2] = "";
 
-        String [][] cars2 = {
-                {"BMW", "Mercedes", "Audi"},
-                {"Volvo", "Ford", "Mazda"},
-                {"Toyota", "Honda", "Ford"}
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter player name please");
 
-        };
+//        try catch if user puts in something else than a string?
+        String playerName = scanner.nextLine();
+        System.out.println("Player name is " + playerName);
 
 
 
-//        Row 0, --->
-        cars[0][0] = "BMW";
-        cars[0][1] = "Mercedes";
-        cars[0][2] = "Audi";
+    }
 
-//        Row 1, colum
-        cars[1][0] = "Volvo";
-        cars[1][1] = "Ford";
-        cars[1][2] = "Mazda";
-
-//        Row 2, column
-        cars[2][0] = "Toyota";
-        cars[2][1] = "Honda";
-        cars[2][2] = "Ford";
-
-//        Skriver ut allt, alla rader, alla kolumner
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println();
-            for (int j = 0; j < cars[i].length; j++) {
-                System.out.print(cars[i][j] + " ");
-            }
-
-        }
-        System.out.println();
-        System.out.println(cars[2][0]); //Toyota
-
+    public void simulatedBoard(String [][] board) {
+        System.out.println(board[0][0] + " " + board[0][1] + " " + board[0][2]);
+        System.out.println(board[1][0] + " " + board[1][1] + " " + board[1][2]);
+        System.out.println(board[2][0] + " " + board[2][1] + " " + board[2][2]);
     }
 }
 
@@ -60,5 +52,6 @@ set properties as private, no need for someone to play around the "board"?, scor
 scanner -> Player 1, what's your name? - enter name
 scanner -> Player 2, what's your name? - enter name
 print out board, change 2d array[][] for actual user input values?
+OOP -> board as a class? interface?
 
  */
