74f493e57e8a7e7b1957827c979997b2704e4f44
nicholassamuelssonjeria <nicholasjobb.jeria@gmail.com>
2024-10-05 15:02:20 +0200

    Minor type changes, from String to char

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
 
-        String[][] board = new String[3][3];
-        board[0][0] = " ";
-        board[0][1] = " ";
-        board[0][2] = " ";
-        board[1][0] = " ";
-        board[1][1] = " ";
-        board[1][2] = " ";
-        board[2][0] = " ";
-        board[2][1] = " ";
-        board[2][2] = " ";
+        char[][] board = new char[3][3];
+        board[0][0] = ' ';
+        board[0][1] = ' ';
+        board[0][2] = ' ';
+        board[1][0] = ' ';
+        board[1][1] = ' ';
+        board[1][2] = ' ';
+        board[2][0] = ' ';
+        board[2][1] = ' ';
+        board[2][2] = ' ';
 
         simulatedBoard(board);
 
@@ -35,8 +36,8 @@ public class Main {
 
     }
 
-//    Method that shows the simulated play board, change to char instead of string?, make private later
-    static public void simulatedBoard(String [][] board) {
+    //    Method that shows the simulated play board, change to char instead of string?, make private later
+    static public void simulatedBoard(char [][] board) {
         System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
         System.out.println("--+---+--");
         System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
@@ -55,8 +56,8 @@ player 1 = x
 player 2 = o
 player 1, your turn..
 player 2 your turn..
-
-if statement/switch, case block -> if square is already taken, return? "square is already taken, try a new one"
+keep track of who's turn it is, boolean values?, player1Turn = true, player2Turn = false ??, compare the values in a method?
+if statement, else if /switch, case block -> if square is already taken, return? "square is already taken, try a new one"
 Nobody won, tie? start over?
 set properties as private, no need for someone to play around the "board"?, scores?
 scanner -> Player 1, what's your name? - enter name
@@ -65,6 +66,8 @@ print out board, change 2d array[][] for actual user input values?
 OOP -> board as a class? interface?, test is FAILED if everything is on main, show that you can use OOP.
 clean up all code before turning it in. try at least
 scanner pressing enter without a value, something something "try again"?
+Change String board to a char? fewer complications? -> research.
+if else code block, to make user input valid? -> if user input is 1 then [0][0] -> x or o, starting from the top right corner
 
 
  */
